9e1fff9e194623bba5c9678498570ac1b41bb7b8
Billy <billychorey@gmail.com>
2024-02-24 17:55:34 -0500

    anagram lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        
+    def match(self, word_list):
+        matches = []
+
+        for word in word_list:
+            lowercase_word = self.word.lower()
+            lowercase_anagram = word.lower()
+
+            if sorted(lowercase_word) == sorted(lowercase_anagram):
+                matches.append(word)
+
+        return matches
